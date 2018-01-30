52bc941f7b93ad9a95f342b91a6f7ccfc8fee0a1
waranchit <waranchit.ch@gmail.com>
2018-01-30 11:44:04 +0700

    Running ReactJs in Docker

---
+++ src/Text.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Text extends Component {
+  render() {
+    return (
+      <div>
+        <h1> THIS is Text {this.props.title}</h1>
+      </div>
+    )
+  }
+}
+export default Text
